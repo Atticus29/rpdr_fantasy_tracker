7d7eecea1301f9df3ea42c20b432650b3d0bbadc
Mark <mark.aaron.fisher@gmail.com>
2020-05-22 19:47:39 -0700

    update for episode preceeding finale

--- TrianglesEverywhere/tracker_s12_TE.R
+++ TrianglesEverywhere/tracker_s12_TE.R
@@ -314,34 +314,34 @@ x = barplot(scoreTable,ylab="Score",xaxt="n", las=2, col=darkcols, ylim=c(0,50))
 text(cex=1, x=x, y=-7.2, currentData[,2], xpd=TRUE, srt=90)
 
 
-# ###########
-# ##Week 12##
-# ###########
-# 
-# fileName = "12_s11_responses_sojuKahannaHoneyMercedesArielScarletRajahPlastiqueShugahNina_removed.csv"
-# revisedFileName = sub('\\.csv','', fileName)
-# data = read.csv(file = paste0(baseDir, fileName), header=T, sep=",", stringsAsFactors = FALSE)
-# 
-# currentData = data
-# currentScores = c(rep(0,nrow(currentData)))
-# tops = c("Yvie Oddly", "A'keria Chanel Davenport", "Silky Nutmeg Ganache")
-# isThereATie = FALSE
-# isThereADoubleSave = FALSE
-# howManyPeopleTied =1
-# 
-# bottoms = c("Vanessa Vanjie Mateo", "Brooke Lynn Hytes")
-# 
-# wk12Scores = calculateNewScores(currentData, currentScores, tops, bottoms, isThereATie, howManyPeopleTied, isThereADoubleSave)
-# 
-# colorRampFunction = colorRampPalette(brewer.pal(12, "Paired"))
-# darkcols <- colorRampFunction(nrow(data)*2)
-# # table(wk1Scores,wk2Scores)
-# # TODO apply sum by column scoreTotal = sum(wk1Scores, wk2Scores, wk3Scores, wk4Scores, wk5Scores, wk6Scores, wk7Scores, wk8Scores, wk9Scores, wk10Scores,wk11Scores, wk12Scores)
-# scoreTable = rbind(wk1Scores, wk2Scores, wk3Scores, wk4Scores, wk5Scores, wk6Scores, wk7Scores, wk8Scores, wk9Scores, wk10Scores, wk11Scores, wk12Scores)
-# colnames(scoreTable) = currentData[,2]
-# x = barplot(scoreTable,ylab="Score",xaxt="n", las=2, col=darkcols, ylim=c(0,50))
-# text(cex=1, x=x, y=-5.7, currentData[,2], xpd=TRUE, srt=90)
-# 
+###########
+##Week 12##
+###########
+
+fileName = "10_ep_TE_RPDR_S12 - 3_ep_TE_RPDR_S12.csv"
+revisedFileName = sub('\\.csv','', fileName)
+data = read.csv(file = paste0(baseDir, fileName), header=T, sep=",", stringsAsFactors = FALSE)
+
+currentData = data
+currentScores = c(rep(0,nrow(currentData)))
+tops = c("Gigi Goode", "Jaida Essence Hall", "Sherry Pie")
+isThereATie = FALSE
+isThereADoubleSave = FALSE
+howManyPeopleTied =1
+
+bottoms = c("Jackie Cox", "Crystal Methyd")
+
+wk12Scores = calculateNewScores(currentData, currentScores, tops, bottoms, isThereATie, howManyPeopleTied, isThereADoubleSave)
+
+colorRampFunction = colorRampPalette(brewer.pal(12, "Paired"))
+darkcols <- colorRampFunction(nrow(data)*2)
+# table(wk1Scores,wk2Scores)
+# TODO apply sum by column scoreTotal = sum(wk1Scores, wk2Scores, wk3Scores, wk4Scores, wk5Scores, wk6Scores, wk7Scores, wk8Scores, wk9Scores, wk10Scores,wk11Scores, wk12Scores)
+scoreTable = rbind(wk3Scores, wk4Scores, wk5Scores, wk6Scores, wk7Scores, wk8Scores, wk9Scores, wk10Scores, wk11Scores, wk12Scores)
+colnames(scoreTable) = currentData[,2]
+x = barplot(scoreTable,ylab="Score",xaxt="n", las=2, col=darkcols, ylim=c(0,50))
+text(cex=1, x=x, y=-8, currentData[,2], xpd=TRUE, srt=90)
+
 # ######################
 # ##Week 13 and finale##
 # ######################
